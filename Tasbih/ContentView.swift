1a10acd9999f2966401d2e0788ee700679ab6659
Nodirbek Kadamov <kadamovnk@icloud.com>
2023-11-11 15:05:25 +0500

    Initial Commit

---
+++ Tasbih/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tasbih
+//
+//  Created by Nodirbek Kadamov on 11/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tasbih/TasbihApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasbihApp.swift
+//  Tasbih
+//
+//  Created by Nodirbek Kadamov on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TasbihApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
